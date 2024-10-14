b49f91b28b5fc96b2c38301082204dc992e98709
Anna Makowski <a.makowski723@gmail.com>
2024-10-15 01:40:18 +0200

    Add Security Configurations

---
+++ src/main/java/com/makowski/user_service/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.makowski.user_service.security;
+
+import com.makowski.user_service.security.filter.AuthenticationFilter;
+import com.makowski.user_service.security.filter.ExceptionHandlerFilter;
+import com.makowski.user_service.security.filter.JWTAuthorizationFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig {
+    private CustomAuthenticationManager customAuthenticationManager;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(customAuthenticationManager);
+        authenticationFilter.setFilterProcessesUrl("/authenticate");
+        http
+                .headers(headers -> headers.frameOptions(Customizer.withDefaults()).disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2/**")).permitAll()
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/swagger-ui/**")).permitAll()
+                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.POST, SecurityConstants.REGISTER_PATH)).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new ExceptionHandlerFilter(), AuthenticationFilter.class)
+                .addFilter(authenticationFilter)
+                .addFilterAfter(new JWTAuthorizationFilter(), AuthenticationFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
--- src/main/java/com/makowski/user_service/service/UserService.java
+++ src/main/java/com/makowski/user_service/service/UserService.java
@@ -3,6 +3,7 @@ package com.makowski.user_service.service;
 import com.makowski.user_service.entity.User;
 import com.makowski.user_service.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,15 +13,22 @@ import java.util.Optional;
 public class UserService {
 
     private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
 
     public User createUser(User user) {
         Optional<User> existingUser = userRepository.findByUsername(user.getUsername());
         if (existingUser.isPresent()) throw new RuntimeException();
                 //change exception!!!!
-        // password encode
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
+    public User findByUsername(String username) {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isPresent()) return user.get();
+        else throw new RuntimeException();
+                //change exception!!!!
+    }
 
 
 }
