115f85a33f0e4d2bb2ad71990b8951116cdcbe87
Anna Makowski <a.makowski723@gmail.com>
2024-10-24 01:46:48 +0200

    mplement token validation logic for inter-service communication

--- src/main/java/com/makowski/user_service/controller/UserController.java
+++ src/main/java/com/makowski/user_service/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.makowski.user_service.controller;
 
-import com.makowski.user_service.dto.TokenRequest;
 import com.makowski.user_service.entity.User;
 import com.makowski.user_service.service.TokenService;
 import com.makowski.user_service.service.UserService;
@@ -8,10 +7,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -27,7 +23,8 @@ public class UserController {
     }
 
     @PostMapping("/validate-token")
-    public ResponseEntity<Boolean> validateToken(@RequestBody TokenRequest tokenRequest) {
-        return ResponseEntity.ok(tokenService.validateToken(tokenRequest));
+    public ResponseEntity<String> validateTokenGetUsername(@RequestHeader("Authorization") String token) {
+        return ResponseEntity.ok(tokenService.validateTokenGetUsername(token));
     }
+
 }
--- src/main/java/com/makowski/user_service/dto/TokenRequest.java
+++
@@ -1,9 +0,0 @@
-package com.makowski.user_service.dto;
-
-import lombok.Data;
-
-@Data
-public class TokenRequest {
-    private String token;
-    private String username;
-}
